2d844c0f5fe1d2a322bf4dcb8c799f5edf3759cf
mtumilowicz <michal.tumilowicz01@gmail.com>
2022-09-16 22:04:44 +0200

    removing padding

--- src/main/scala/app/ZCipher.scala
+++ src/main/scala/app/ZCipher.scala
@@ -8,11 +8,12 @@ import zio.{RIO, ZIO}
 
 object ZCipher {
 
-  def encrypt(keyAlias: String, dataToEncrypt: String):
+  def encrypt(keyAlias: String, data: String):
   ZIO[Session with UserStateContext.LoggedIn, Throwable, Array[Byte]] = ZIO.scoped {
     for {
       secretKey <- retrieveKey(keyAlias)
       mechanism = Mechanism.get(PKCS11Constants.CKM_AES_ECB)
+      dataToEncrypt = data.length.toString + "||" + data
       bytes = dataToEncrypt.getBytes("utf-8")
       encryption <- encrypt(bytes, secretKey, mechanism)
     } yield encryption
@@ -23,8 +24,11 @@ object ZCipher {
     for {
       secretKey <- retrieveKey(keyAlias)
       mechanism = Mechanism.get(PKCS11Constants.CKM_AES_ECB)
-      decryption <- decrypt(dataToDecrypt, secretKey, mechanism, padding(dataToDecrypt.length).length)
-    } yield decryption
+      bytes <- decrypt(dataToDecrypt, secretKey, mechanism)
+      asString = new String(bytes)
+      length = asString.takeWhile(_ != '|').toInt
+      data = asString.dropWhile(_ != '|').slice(2, length + 2)
+    } yield data.getBytes("utf-8")
   }
 
   private def encrypt(data: Array[Byte],
@@ -34,28 +38,21 @@ object ZCipher {
     session <- ZIO.service[Session]
     _ <- ZIO.fail(new RuntimeException("Key retrieval error")).unless(encryptionMechanism.isSingleOperationEncryptDecryptMechanism || encryptionMechanism.isFullEncryptDecryptMechanism)
     _ <- ZIO.attemptBlocking(session.encryptInit(encryptionMechanism, encryptionKey))
-    iv = padding(data.length)
-    toEncrypt = iv ++ data
-    chunkSize = 16 + (toEncrypt.length / 16) * 16
-    outBuffer = Array.ofDim[Byte](toEncrypt.length)
-    _ <- ZIO.attemptBlocking(session.encrypt(toEncrypt, 0, chunkSize, outBuffer, 0, chunkSize))
+    chunkSize = 1024
+    outBuffer = Array.ofDim[Byte](chunkSize)
+    _ <- ZIO.attemptBlocking(session.encrypt(data, 0, chunkSize, outBuffer, 0, chunkSize))
   } yield outBuffer
 
   private def decrypt(data: Array[Byte],
                       decryptionKey: Key,
-                      decryptionMechanism: Mechanism,
-                      paddingFirstBytes: Int):
+                      decryptionMechanism: Mechanism):
   RIO[Session with UserStateContext.LoggedIn, Array[Byte]] = for {
     session <- ZIO.service[Session]
     _ <- ZIO.fail(new RuntimeException("Key retrieval error")).unless(decryptionMechanism.isSingleOperationEncryptDecryptMechanism || decryptionMechanism.isFullEncryptDecryptMechanism)
     _ <- ZIO.attemptBlocking(session.decryptInit(decryptionMechanism, decryptionKey))
-    chunkSize = 16 + (data.length / 16) * 16
+    chunkSize = 1024
     outBuffer = Array.ofDim[Byte](chunkSize)
     _ <- ZIO.attemptBlocking(session.decrypt(data, 0, chunkSize, outBuffer, 0, chunkSize))
-  } yield outBuffer.slice(paddingFirstBytes, Integer.parseInt(outBuffer.take(paddingFirstBytes).mkString, 2) + paddingFirstBytes)
-
-  private def padding(i: Int): Array[Byte] = {
-    Integer.toBinaryString((1 << 5) | i).map(_ - '0').map(_.toByte).drop(1).toArray
-  }
+  } yield outBuffer
 
 }
